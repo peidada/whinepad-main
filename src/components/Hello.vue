24ab1c679eedbf3241d6cef7ab6962cf9cc67716
pei <18834003051@163.com>
2017-07-20 02:09:09 +0800

    vue

---
+++ src/components/Hello.vue
@@ -0,0 +1,212 @@
+<template>
+  <div>
+    <header class='header'>
+      <img src="../assets/wp-logo2.png" alt=""> Welcome to Whinepad!
+    </header>
+    <section class='box'>
+      <header class='boxTop'>
+        <div class='add btn' @click='addActive=true'>+ add</div>
+        <input type="text" class='search' placeholder="search" v-model='search'>
+      </header>
+      <table>
+        <thead>
+          <tr>
+            <th class='name'>Name</th>
+            <th class='year'>Year</th>
+            <th class='grape'>Grape</th>
+            <th class='rating'>Rating</th>
+            <th class='action'>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for='(item,index) in list' :key='item'>
+            <td class='name'>{{item.name}}</td>
+            <td class='year'>{{item.year}}</td>
+            <td class='grape'>{{item.grape}}</td>
+            <td class='rating'>
+              <Star :score='item.rating' disabled></Star>
+            </td>
+            <td>
+              <div class='action'>
+                 <span @click='handleInfo(item)'>查看</span>
+                 <span @click='handleEdit(item,index)'>编辑</span>
+                 <span @click='handleDel(item,index)'>删除</span>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
+    <AlertBox v-if='addActive' @add='handleAdd' @cancle='addActive=false' title='Add new item' type='add'></AlertBox>
+    <AlertBox v-if='InfoActive'  @cancle='InfoActive=false' title='Item info' type='info' :info='info'></AlertBox>
+    <AlertBox v-if='EditActive' @add='edit' @cancle='EditActive=false' title='Edit item' type='edit' :info='info'></AlertBox>
+    <AlertBox v-if='DeleteActive' @del='del' @cancle='DeleteActive=false' title='Confirm deletion' type='del' :info='info'></AlertBox>
+  </div>
+</template>
+
+<script>
+import Star from './star.vue'
+import AlertBox from './alertBxo.vue'
+export default {
+  name: 'hello',
+  data () {
+    return {
+      addActive: false,
+      InfoActive: false,
+      EditActive: false,
+      DeleteActive: false,
+      info: {},
+      list: [],
+      index: '',
+      search: ''
+    }
+  },
+  computed: {
+    Searchlist () {
+      const arr = []
+      const vm = this
+      this.list.forEach(function (item) {
+        if (vm.search === item.name && vm.search !== '') {
+          arr.push(item)
+        }
+      })
+      return arr
+    }
+  },
+  components: {
+    'Star': Star,
+    'AlertBox': AlertBox
+  },
+  methods: {
+    handleAdd (val) {
+      this.list.push(val)
+      this.addActive = false
+    },
+    handleInfo (item) {
+      this.info = item
+      this.InfoActive = true
+    },
+    handleEdit (item, index) {
+      this.info = item
+      this.index = index
+      this.EditActive = true
+    },
+    handleDel (item, index) {
+      this.index = index
+      this.info = item
+      this.DeleteActive = true
+    },
+    edit (val) {
+      this.list[this.index] = val
+      this.EditActive = false
+    },
+    del () {
+      this.DeleteActive = false
+      this.List.splice(this.index, 1)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header {
+  width: 100%;
+  height: 120px;
+  background: #6f001b;
+  display: flex;
+  align-items: center;
+  color: #fff;
+  font-family: Norican, cursive;
+  padding: 20px;
+  font-size: 50px;
+}
+
+.header img {
+  margin-right: 20px;
+}
+
+.box {
+  padding: 40px;
+}
+
+.boxTop {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.btn {
+  width: 110px;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  font-size: 20px;
+  background: #6f001b;
+  border-radius: 50px;
+  color: #fff;
+  font-weight: 700;
+  font-size: 30px;
+  padding: 0 10px;
+  cursor: pointer;
+}
+
+.search {
+  width: 200px;
+  height: 40px;
+  outline: none;
+  padding: 10px;
+}
+
+table {
+  width: 100%;
+  margin-top: 20px;
+}
+
+table th {
+  font-weight: normal;
+  color: #7f7f7f;
+  font-size: 13px;
+  height: 30px;
+}
+table td{
+  border-right:1px solid #F0F0F0; 
+  background: #fff;
+  padding:0 10px;
+  height: 58px;
+}
+table td:last-child{
+  border-right:none; 
+}
+thead .name {
+  width: 15%;
+}
+
+thead .year {
+  width: 10%;
+}
+
+thead .grape {
+  width: 15%;
+}
+
+thead .rating {
+  width: 25%;
+}
+
+thead .action {
+  width: 35%;
+}
+tbody .name{
+  font-weight: 700;
+}
+tbody .year{
+   color: #7f7f7f;
+   text-align: center;
+}
+tbody .grape{
+  font-weight: 700;
+  color: #6f001b;
+}
+</style>
---
+++ src/components/alertBxo.vue
@@ -0,0 +1,206 @@
+<template>
+  <div class='alertBox' ref='box'>
+       <section class='boxInner'>
+          <header class='header'>
+            {{title}}
+          </header>
+          <section class='center' v-if='type=="add"||type=="edit"'>
+             <form>
+                <div>
+                   <label for="">Name</label>
+                   <input type="text" v-model="add.name">
+                </div>
+                <div>
+                   <label for="">Year</label>
+                   <input type="number" placeholder="2017" min='1900' v-model="add.year">
+                </div>
+                <div>
+                   <label for="">Grape</label>
+                   <select name="" id="" v-model="add.grape">
+                      <option :value="item.value" v-for='item in selectList' :key='item'>{{item.value}}</option>
+                   </select>
+                </div>
+                <div>
+                   <label for="">Rating</label>
+                   <Star :score='rating' @starLevel='getLevel'></Star>
+                </div>
+                <div>
+                   <label for="">Commits</label>
+                   <textarea name="" id="" cols="20" rows="4" v-model="add.commit"></textarea>
+                </div>
+             </form>
+          </section>
+          <section v-else-if="type=='info'" class='center'>
+             <form>
+                <div>
+                   <label for="">Name</label>
+                   <div class='item'>{{info.name}}</div>
+                </div>
+                <div>
+                   <label for="">Year</label>
+                    <div class='item'>{{info.year}}</div>
+                </div>
+                <div>
+                   <label for="">Grape</label>
+                    <div class='item'>{{info.grape}}</div>
+                </div>
+                <div>
+                   <label for="">Rating</label>
+                   <Star :score='info.rating'></Star>
+                </div>
+                <div>
+                   <label for="">Commits</label>
+                    <div class='item'>{{info.commit}}</div>
+                </div>
+             </form>
+          </section>
+           <section v-else-if="type=='del'" class='center'>
+              <div>Are you sure you want to delete "{{info.name}}"?</div>
+          </section>
+          <footer class='footer'  v-if='type=="add"||type=="edit"'>
+             <span @click='cancle'>Cancel</span>
+             <div v-if='type=="add"' class='btn' @click='addEvent'>Add</div>
+             <div v-else-if='type=="edit"' class='btn' @click='addEvent'>Save</div>
+          </footer>
+          <footer class='footer'  v-if='type=="del"'>
+             <span @click='cancle'>Cancel</span>
+             <div class='btn' @click='delEvent'>Delete</div>
+          </footer>
+           <footer class='footer'  v-else-if='type=="info"'>
+             <div class='btn' @click='cancle'>OK</div>
+          </footer>
+       </section>
+  </div>
+</template>
+<script>
+import Star from './star.vue'
+export default {
+  data () {
+    return {
+      selectList: [
+        {value: 'aaaaa'},
+        {value: 'bbbbb'},
+        {value: 'ccccc'},
+        {value: 'ddddd'},
+        {value: 'eeeee'}
+      ],
+      add: {
+        name: '',
+        year: '',
+        grape: '',
+        rating: '',
+        commit: ''
+      }
+    }
+  },
+  mounted () {
+    this.$refs.box.style.height = document.documentElement.clientHeight + 'px'
+    if (this.type === 'edit') {
+      this.add = this.info
+    }
+    console.log(this.add.rating)
+  },
+  components: {
+    Star
+  },
+  props: ['title', 'type', 'info'],
+  methods: {
+    addEvent () {
+      this.$emit('add', this.add)
+    },
+    cancle () {
+      this.$emit('cancle')
+    },
+    getLevel (val) {
+      this.add.rating = val
+    },
+    delEvent () {
+      this.$emit('del')
+    }
+  },
+  computed: {
+    rating () {
+      return this.add.rating
+    }
+  }
+}
+</script>
+<style scoped>
+ .alertBox{
+     width: 100%;
+     height: 100%;
+     background: rgba(0,0,0,.5);
+     position: absolute;
+     left: 0;
+     top: 0;
+ }
+ .boxInner{
+   width: 350px;
+   height: 500px;
+   position: absolute;
+   left:0;top:0;
+   right: 0;bottom: 0;
+   margin: auto;
+ }
+ .header {
+  width: 100%;
+  height: 80px;
+  background: #6f001b;
+  display: flex;
+  align-items: center;
+  color: #fff;
+  font-family: Norican, cursive;
+  padding: 20px;
+  font-size: 40px;
+}
+.btn {
+  width: 70px;
+  height: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  font-size: 20px;
+  background: #6f001b;
+  border-radius: 50px;
+  color: #fff;
+  font-weight: 700;
+  font-size: 20px;
+  padding: 0 10px;
+  cursor: pointer;
+  margin-left:10px;
+}
+.footer{
+  width: 100%;
+  height: 80px;
+  background: #fff;
+  border-top: 1px solid #ccc;
+  display: flex;
+  justify-content: flex-end;
+  align-items:center;
+  padding: 0 10px;
+}
+.footer span{
+  cursor: pointer;
+}
+.center{
+  max-height: 340px;
+  overflow-y: auto;
+  background: #fff;
+  padding: 10px 30px;
+}
+.center label{
+  width: 100%;
+  display: block;
+  font-size:13px;
+  color: #7f7f7f;
+  margin-bottom:5px;
+  margin-top:10px;
+}
+.center  input,.center  select{
+  padding: 10px;
+  outline: none;
+  width: 180px;
+}
+</style>
+
+
